b09163aed31d2c2f34661edd6e2ad1def763b6c8
Kelvem Carlos <kelvemcif@gmail.com>
2022-08-23 16:02:06 -0300

    API farmácia

---
+++ farmacia/farmacia/src/main/java/com/api/farmacia/produtos/models/Produto.java
@@ -0,0 +1,84 @@
+package com.api.farmacia.produtos.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+
+public class Produto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) //Indicando que o banco irá criar de forma automática id.
+	private long id;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private BigDecimal quantidade;
+	
+	private BigDecimal valor;
+	
+	private String tipo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+	
+}
---
+++ farmacia/farmacia/src/main/java/com/api/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.api.farmacia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.farmacia.produtos.models.Produto;
+
+//Usei o JpaRepository, porque ele é bom para fazer persistência no bd.
+//save, delete, já vem prontos.
+public interface ProdutoRepository  extends JpaRepository<Produto, Long>{
+	
+	
+	Produto findById(long id);
+}
